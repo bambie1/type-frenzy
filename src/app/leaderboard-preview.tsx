5be7e8fac9cbf584efa9aa3c87761ad902bd7538
Benaiah Barango <benaiah.barango@fullscript.com>
2023-06-15 08:27:47 -0400

    RSCs not playing nice

--- src/app/leaderboard-preview.tsx
+++ src/app/leaderboard-preview.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { databases } from "@/utils/appwrite";
+import { Query } from "appwrite";
 
 import Table from "./table";
 
@@ -9,11 +14,33 @@ export interface Leaderboard {
   average_score: number;
 }
 
-interface Props {
-  leaderboard: Leaderboard[];
-}
+const LeaderboardPreview = () => {
+  const [leaderboard, setLeaderboard] = useState<Leaderboard[]>([]);
 
-const LeaderboardPreview = ({ leaderboard }: Props) => {
+  useEffect(() => {
+    getLeaderboardData();
+  }, []);
+
+  async function getLeaderboardData() {
+    const { documents: leaderboard } = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "",
+      process.env.NEXT_PUBLIC_APPWRITE_SCORES_COLLECTION_ID || "",
+      [Query.orderDesc("average_score")]
+    );
+
+    setLeaderboard(leaderboard as any);
+  }
+
+  if (!leaderboard.length)
+    return (
+      <div>
+        <div className="mt-8 h-48 w-full animate-pulse rounded-lg bg-gray-200"></div>
+        <Link href="/leaderboard" className="mt-6 inline-flex underline">
+          See full leaderboard
+        </Link>
+      </div>
+    );
+
   return (
     <div className="mt-8 flow-root">
       <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
